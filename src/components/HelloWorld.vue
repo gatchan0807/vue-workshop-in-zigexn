31178873f35714d280a51443d03068addd9cc1f8
ahaha0807 <b5331@oic.jp>
2018-07-08 21:25:47 +0900

    ランダムな値を返す算出プロパティを作る

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ randomNumber }}</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  computed: {
+    randomNumber: function() {
+      return Math.floor( Math.random() * 100 ) // 0〜100のランダムな値を返す
+    }
+  },
   data () {
     return {
       msg: 'このアプリは最高のアプリさ！'
