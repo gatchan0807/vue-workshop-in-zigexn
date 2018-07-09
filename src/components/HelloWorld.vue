6db7a6fd54caf3348d43411480d611669793bb1e
gatchan0807 <yuki.dolphin.koga@gmail.com>
2018-07-09 20:57:26 +0900

    フォームの追加とボタンのデザインの修正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,11 @@
     <h2 v-if="randomNumber >= 50">50以上やで</h2>
     <h2 v-if="randomNumber < 50">50より小さいで</h2>
 
+    <form onsubmit="return false;">
+      <input type="text">
+      <button type="submit">追加</button>
+    </form>
+
     <ul>
       <li v-for="todo in todos" v-bind:key="todo.contents">
         <input type="checkbox" name="status" id="status" v-model="todo.status">
@@ -63,5 +68,11 @@ a {
 
 .done {
   text-decoration: line-through;
+}
+
+button {
+  background-color: #42b983;
+  color: #fff;
+  border: 0;
 }
 </style>
